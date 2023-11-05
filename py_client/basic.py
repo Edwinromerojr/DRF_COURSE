d6f2e16db528a44c92b53295c9621aedeeb7c108
Edwin Romero <edwinrmrjr@gmail.com>
2023-11-05 09:45:01 +0800

    Add products app

--- py_client/basic.py
+++ py_client/basic.py
@@ -2,7 +2,7 @@ import requests
 
 # endpoint = "https://httpbin.org/status/200/"
 # endpoint = "https://httpbin.org/anything"
-endpoint = "http://127.0.0.1:8000/api" #http://127.0.0.1:8000/
+endpoint = "http://127.0.0.1:8000/api/" #http://127.0.0.1:8000/
 
 get_response = requests.get(endpoint, params={"abc": 123}, json={"query": "hello world"}) # HTTP Request
 # print(get_response.headers)
