d3e23878810b19a734dab1c925090526e240fa88
Edwin Romero <edwinrmrjr@gmail.com>
2023-11-07 15:31:59 +0800

    CreateAPIView

--- backend/products/views.py
+++ backend/products/views.py
@@ -4,9 +4,23 @@ from .models import Product
 from .serializers import ProductSerializer
 
 
-class ProductDetailAPIView(generics.RetrieveAPIView):
+class ProductCreateAPIView(generics.CreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
+    def perform_create(self, serializer):
+        # serializer.save(user=self.request.user)
+        print(serializer.validated_data)
+        title = serializer.validated_data.get('title')
+        content = serializer.validated_data.get('content') or None
+        if content is None:
+            content = title
+        serializer.save(content=content)
+
+product_create_view = ProductCreateAPIView.as_view()
+
+class ProductDetailAPIView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
 
 product_detail_view = ProductDetailAPIView.as_view()
