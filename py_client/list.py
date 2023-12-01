a27357bea1b4b2df6ccf7704f6fa2b21c17f32b1
Edwin Romero <edwinrmrjr@gmail.com>
2023-12-01 23:42:35 +0800

    Pagination

--- py_client/list.py
+++ py_client/list.py
@@ -19,3 +19,10 @@ if auth_response.status_code == 200:
     get_response = requests.get(endpoint, headers=headers) # HTTP Request
 
     print(get_response.json())
+    data = get_response.json()
+    next_url = data['next']
+    results = data['results']
+    print("next_url", next_url)
+    print(results)
+    # if next_url is not None:
+    #     get_response = requests.get(next_url, headers=headers)
